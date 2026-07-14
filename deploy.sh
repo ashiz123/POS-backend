2faecf9404ca5b0b3ac4f1a61dedb218ab1c78cc
Ashiz <ashizweb@gmail.com>
2026-07-14 09:12:14 +0100

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Auto-generate a version tag
 VERSION=$(date +%Y%m%d-%H%M)
 
 echo "Building and pushing version"
 
-# Use -f to point to the file, and change '.' to 'backend'
-# to set the 'context' (where your source code lives)
 docker buildx build \
   --platform linux/amd64 \
   --target production \
